0b1923cdc3da0108ddfeb11b035014a21fbe1248
unknown <daudsutanto13@gmail.com>
2022-01-20 01:04:19 +0700

    fix some issue

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,40 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const login = async (req, res) => {
   try {
     const user = await User.findOne({
       email: req.body.email.toLowerCase(),
     }).exec();
-    const password = bcrypt.compareSync(req.body.password, user.password);
+    const authenticated = bcrypt.compareSync(req.body.password, user.password);
     if (!user) return res.json({ message: "something went wrong" });
-    if (!password) return res.json({ message: "something went wrong" });
-    return res.status(200).json({ message: "success login" });
+    if (!authenticated) return res.json({ message: "something went wrong" });
+    _user = {
+      _id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      roninAddress: user.roninAddress,
+      isAdmin: user.isAdmin,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    };
+    const token = jwt.sign(
+      {
+        _user,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "30m" }
+    );
+    return res.status(200).json({
+      message: "success login",
+      user: _user,
+      token,
+    });
   } catch (error) {
     console.log(error);
-    res.json({ message: "error login" });
+    return res.json({ message: "error login" });
   }
 };
 
@@ -22,7 +44,10 @@ const register = async (req, res) => {
       email: req.body.email.toLowerCase(),
     }).exec();
     if (user) return res.json({ message: "email registered" });
-    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+    const hashedPassword = bcrypt.hashSync(
+      req.body.password,
+      process.env.SALT_ROUNDS
+    );
     const instance = new User({
       firstName: req.body.firstName,
       lastName: req.body.lastName,
@@ -34,7 +59,7 @@ const register = async (req, res) => {
     return res.status(201).json({ message: "success register" });
   } catch (error) {
     console.log(error);
-    res.json({ message: error.errors });
+    return res.json({ message: error.errors });
   }
 };
 
