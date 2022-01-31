8735890920f60f25cbf2c4079868de9acf3ff5f6
unknown <daudsutanto13@gmail.com>
2022-01-31 11:59:32 +0700

    change mongodb URI

--- database/database-connect.js
+++ database/database-connect.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const mongodb_uri = process.env.MONGO_DB_URI;
+const mongodb_uri = `mongodb+srv://daud13:${process.env.MONGO_DB_PASS}@cluster0.lucj8.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`;
 
 const dbConnect = async () => {
   try {
